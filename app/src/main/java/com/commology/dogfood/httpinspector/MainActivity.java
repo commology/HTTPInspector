90f80d7ad4cdea2b00a3b364bf80fb0f801acddc
commology <commology@gmail.com>
2019-08-01 15:47:49 +0800

    Initial commit

---
+++ app/src/main/java/com/commology/dogfood/httpinspector/MainActivity.java
@@ -0,0 +1,13 @@
+package com.commology.dogfood.httpinspector;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
